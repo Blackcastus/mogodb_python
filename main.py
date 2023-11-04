89d666601be35786fc422a7c79af60534e8cf477
blackcastus <blackcastus@gmail.com>
2023-11-04 16:32:26 +0700

    get, push, update data of server

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-import pymongo
-import random
+import uvicorn
+from fastapi import FastAPI
 from pydantic import BaseModel
-import json
-import time
+import pymongo
+
+app = FastAPI()
 
 myclient = pymongo.MongoClient("mongodb+srv://iot:iot@cluster0.ftx0nvn.mongodb.net/?retryWrites=true&w=majority")
 mydb = myclient["Mydata"]
 mycol = mydb["sensordata"]
+id = 0
 
-def GetData():
-    return random.randint(0,99)
+class Item(BaseModel):
+    data1: float
+    data2: float
 
-def PostData(temp, humi):
-    mydict = {
-        "temp": temp,
-        "humi": humi
+@app.get("/")
+def root():
+    return {"message": "Hello Word!"}
+
+@app.get("/updates")
+async def update_data(data1: float, data2: float):
+    return {"temp: ": data1, "humi: ": data2}
+
+@app.get("/get")
+async def update_data():
+    x = mycol.find_one()
+    data_return = {
+        "temp: ": x['temp'],
+        "humi: ": x['humi']
     }
-    print(mydict)
+    print(data_return)
+    return data_return
+
+@app.post("/update_post")
+async def update_data_post(item: Item):
+    mydict = {
+        "temp": int(item.data1),
+        "humi": int(item.data2)
+    }
+    # print(mydict)
     mycol.insert_one(mydict)
     return "OK"
 
-while True:
-    data_temp = GetData()
-    data_humi = GetData()
-
-    print(PostData(data_temp, data_humi))
-
-    time.sleep(50)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
